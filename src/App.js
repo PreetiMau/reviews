cc5d21ff4096f49e3fdd467218740fbf2168dbfe
candlesticksm <gpreetiwd@gmail.com>
2021-01-23 00:01:06 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+import Api from './component/Api';
+
+function App() {
+  return (
+    <div>
+      {/* <Forms /> */}
+      <Api/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Display.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+// import Api from './Api';
+
+
+// const google = window.google;
+
+export default class Reviews extends React.Component{
+
+  // componentDidMount =()=>{
+    
+  
+   
+    
+  // }
+
+  render(){
+  
+    // Api();
+    return(
+     <>
+        <h1> hello reviews </h1>
+                
+                  
+    </>
+     )
+    }
+  }
